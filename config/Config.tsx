b5935280c04a8a76320d71e49daded2c020c2786
johannes <johannes.muljana@ait.nsw.edu.au>
2024-06-26 17:18:23 +1000

    added Config

---
+++ config/Config.tsx
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDSippIsJzOiXzMHDkmT9FXMAmkJ_2HEb0",
+    authDomain: "testdata-4f531.firebaseapp.com",
+    projectId: "testdata-4f531",
+    storageBucket: "testdata-4f531.appspot.com",
+    messagingSenderId: "1023637349740",
+    appId: "1:1023637349740:web:6f8e312c9f1359a0b57f49"
+}
